ebc8cfa62d7599dae0ebe8aea17c598295d0371e
yy <2289150297@qq.com>
2023-05-26 19:03:23 +0800

    [ADD] 加入user系列的功能

--- target/generated-sources/annotations/com/example/demo/config/ConverterImpl.java
+++ target/generated-sources/annotations/com/example/demo/config/ConverterImpl.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-05-23T13:14:36+0800",
+    date = "2023-05-26T18:43:53+0800",
     comments = "version: 1.5.2.Final, compiler: javac, environment: Java 1.8.0_74 (Oracle Corporation)"
 )
 public class ConverterImpl extends Converter {
